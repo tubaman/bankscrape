e184b93f2665be0c887adf2bc97b6a8dd62a87ba
Ryan Nowakowski <tubaman@fattuba.com>
2012-12-16 19:09:52 -0600

    bump version and fix deps

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='bankscrape',
-    version='1.0',
+    version='1.1',
     description="Web Scrapers for Financial Institutions",
     author="Ryan Nowakowski",
     author_email="tubaman@fattuba.com",
     packages=['bankscrape', 'bankscrape.banks'],
     scripts=['scripts/bankscrape'],
-    install_requires=['BeautifulSoup>=3.1'],
+    install_requires=['BeautifulSoup>=4', 'requests'],
     )
